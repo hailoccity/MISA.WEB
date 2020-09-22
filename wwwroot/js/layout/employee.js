afbf778c147f6e78267cdb7d0826169ef4603e4a
Le Doan Hieu <ledoanhieu1997@gmail.com>
2020-09-22 15:31:32 +0700

    complete

---
+++ wwwroot/js/layout/employee.js
@@ -0,0 +1,6 @@
+function showDialog(){
+    document.getElementsByClassName("orm-dialog")[0].style.display="block";
+}
+function colseDialog(){
+    document.getElementsByClassName("btn-close")[0].style.display="none"
+}
