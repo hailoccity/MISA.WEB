c446f1b400628ac033a9f1d7f370cd93f5514ed5
hailoccity <ledoanhieu1997@gmail.com>
2020-09-24 22:03:19 +0700

    them chuc nang Them khách hàng

--- wwwroot/js/page/customer.js
+++ wwwroot/js/page/customer.js
@@ -8,10 +8,26 @@ $(document).ready(function (){
  */
 class CustomerJS{
     constructor(){
+  
+
         this.loadData();
+        $("#btnAdd").click(function(){
+            $("#frDialog").show();
+        })
+        $("#btnClose").click(function(){
+            $("#frDialog").hide();
+        
+        })
+        $("#btnCloseX").click(function(){
+            $("#frDialog").hide();
+        
+        })
+        $("#btnSave").click(this.saveData.bind(this));
     }
+           
 
  loadData(){
+    $('table#tblistCustomer tbody').empty();
   //lay du lieu ve
   var data = fakeData;
   $.each(data,function(index, item){
@@ -30,6 +46,35 @@ class CustomerJS{
       debugger;
   })
 }
+//
+saveData(){
+    //luu dữ liệu
+    var customerName = $("#txtCustomerName").val(),
+    customerCode = $("#txtCustomerCode").val(),
+    birthday =$("#dtBirth").val(),
+    mobile= $("#txtMobile").val(),
+    debitAmount =$("#txtDebiAmount").val(),
+    check5food =$("#check5Food").is(':checked')
+// từ dữ liệu thu thập được
+    var custumer = {
+        CustomerCode: customerCode,
+        CustomerName: customerName,
+        BirthDay: new Date(birthday),
+        PhoneOfNumber: mobile,
+        DebitAmount: debitAmount,
+        Is5FoodMember: check5food
+    }
+   
+// lưu vào database
+    fakeData.push(custumer);
+// Hiển thị thông báo thành công
+    alert("Thêm thành công");
+//đóng form
+    $("#frDialog").hide();
+//load lại dữ liệu
+    this.loadData();
+    debugger;
+}
 }
 //Dinh dang hien thi tien
 
@@ -40,9 +85,10 @@ var fakeData = [
         BirthDay: new Date(1997,5,2),
         PhoneOfNumber: "0392856231",
         DebitAmount: 1000000,
-        Is5FoodMember: false
+        Is5FoodMember: true
 
     },
+    
     {
         CustomerCode: "KH002",
         CustomerName: "Le Van Dong",
@@ -52,15 +98,6 @@ var fakeData = [
         Is5FoodMember: false
 
     },
-    {
-        CustomerCode: "KH003",
-        CustomerName: "Le Doan Hieu",
-        BirthDay: "20/05/1997",
-        PhoneOfNumber: "0392856231",
-        DebitAmount: 1000000,
-        Is5FoodMember: true
-    }
 
-    
 
 ]
